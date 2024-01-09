75ea32076c298a409cdc5f6028a932c40ed0a733
Ugwu isaac <57339195+Pchib@users.noreply.github.com>
2024-01-09 10:56:46 +0100

    new 8

--- components/Pages/About.tsx
+++ components/Pages/About.tsx
@@ -8,18 +8,18 @@ export default class About extends Component {
             <div>
                 {/* <main id="main"> */}
 
-	<div class="blob-container">
-    <div class="blob"></div>
-    <div class="blob one"></div>
-    <div class="blob two"></div>
-    <div class="blob three"></div>
-    <div class="blob four"></div>
-    <div class="blob five"></div>
-    <div class="blob six"></div>
-    <div class="blob seven"></div>
-    <div class="blob eight"></div>
-    <div class="blob nine"></div>
-    <div class="blob ten"></div>
+	<div className="blob-container">
+    <div className="blob"></div>
+    <div className="blob one"></div>
+    <div className="blob two"></div>
+    <div className="blob three"></div>
+    <div className="blob four"></div>
+    <div className="blob five"></div>
+    <div className="blob six"></div>
+    <div className="blob seven"></div>
+    <div className="blob eight"></div>
+    <div className="blob nine"></div>
+    <div className="blob ten"></div>
   </div>
 
  
